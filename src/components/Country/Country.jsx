b0c0a968654440912437bb27670228025b8d0c97
Sojib Das <contact.sojibdas@gmail.com>
2024-02-14 23:30:33 +0600

    Button Add Visite

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,35 @@
-import './Country.css'
+import { useState } from "react";
+import "./Country.css";
 
-const Country = ({country}) => {
-    const {name, flags, population, area} = country;
-    return (
-        <div className='country colume'>
-            <h3>Name : {name.common} </h3>
-            <img src={flags.png} alt="" />
-            <p>Population : {population}</p>
-            <p>Area {area}</p>
-        </div>
-    );
+const Country = ({ country }) => {
+  const { name, flags, population, area, cca3 } = country;
+  const [visited, setVisited] = useState(false);
+  const [color, setColor] = useState(true);
+  const [buttonColor, setButtonColor] = useState(false);
+
+  const handleVisite = () => {
+    setVisited(!visited);
+    setColor(!color);
+    setButtonColor(!buttonColor)
+  };
+
+  return (
+    <div className="country colume">
+      <h3>Name : {name.common} </h3>
+      <img src={flags.png} alt="" />
+      <p>Population : {population}</p>
+      <p>Area {area}</p>
+      <p>CCA3: {cca3}</p>
+      <button
+        style={{ backgroundColor: color ? "black" : "green", outline: "none" , color: buttonColor ? 'white': 'red'}}
+        onClick={handleVisite}
+      >
+        {visited ? " Visited" : "Visit"}
+      </button>
+      <p>{visited ? " I have visit this country" : " I want to visit the country"}</p>
+      
+    </div>
+  );
 };
 
 export default Country;
